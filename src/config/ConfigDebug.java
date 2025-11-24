6f0049d416c8d999b9c3c57d8142ef88c63b5770
luis <roberdoogarcia@gmail.com>
2025-11-24 19:43:27 -0300

    add ConfigDebug class and Eu.class file

--- src/config/ConfigDebug.java
+++ src/config/ConfigDebug.java
@@ -1,7 +1,7 @@
-package src.a;
+package src.config;
 
 
-public class Eu {
+public class ConfigDebug {
     public static final boolean DEBUG_TB_SIMBOLOS = true;
     public static final boolean DEBUG_ALL = true;
     public static final boolean DEBUG_ANALIZE_SEMANTICA = true;
